46f2c82fad715a78a98f5a54f8961ecde1a2de03
Mario Barragán <albeto8@gmail.com>
2017-06-29 18:36:57 -0500

    Fragments initial setup

---
+++ app/src/main/java/com/example/mariobarragan/devslopesradio/adapters/StationsAdapter.java
@@ -0,0 +1,8 @@
+package com.example.mariobarragan.devslopesradio.adapters;
+
+/**
+ * Created by mariobarragan on 6/29/17.
+ */
+
+public class StationsAdapter {
+}
--- app/src/main/java/com/example/mariobarragan/devslopesradio/fragments/StationsFragment.java
+++ app/src/main/java/com/example/mariobarragan/devslopesradio/fragments/StationsFragment.java
@@ -3,6 +3,8 @@ package com.example.mariobarragan.devslopesradio.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -60,7 +62,16 @@ public class StationsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_stations, container, false);
+        View v = inflater.inflate(R.layout.fragment_stations, container, false);
+
+        RecyclerView recyclerView = (RecyclerView)v.findViewById(R.id.recycler_stations);
+        recyclerView.setHasFixedSize(true);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
+        layoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
+        recyclerView.setLayoutManager(layoutManager);
+
+        return v;
     }
 
 }
